89d95d8fc3adceba75230b8f98ca09d50f7cf742
Thibaut Durand <durand.tibo@gmail.com>
2025-12-22 01:13:08 -0800

    Update tests and deps (#177)

--- tests/functional/test_package.py
+++ tests/functional/test_package.py
@@ -2,9 +2,7 @@ from __future__ import annotations
 
 import pytest
 from feu.imports import is_package_available
-from feu.testing import (
-    torch_available,
-)
+from feu.testing import numpy_available
 
 
 @pytest.fixture(autouse=True)
@@ -12,8 +10,8 @@ def _reset_cache() -> None:
     is_package_available.cache_clear()
 
 
-@torch_available
-def test_torch() -> None:
-    import torch  # local import because it is an optional dependency
+@numpy_available
+def test_numpy() -> None:
+    import numpy as np  # local import because it is an optional dependency
 
-    torch.testing.assert_close(torch.ones(2, 3) + torch.ones(2, 3), torch.full((2, 3), 2.0))
+    assert np.array_equal(np.ones((2, 3)) + np.ones((2, 3)), np.full((2, 3), 2.0))
